49f2fc796832be87b11ae159fca96602c9bbb944
Øxonovo <mahalosaint@gmail.com>
2025-02-23 23:47:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,3 +15,13 @@ menu_btn.addEventListener('click', function() {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 });
+
+  // Function to detect mobile or tablet devices
+function isMobileOrTablet() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Redirect mobile and tablet users to a different webpage
+if (isMobileOrTablet()) {
+  window.location.href = "mobile.php"; // Replace with your desired URL
+}
