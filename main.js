082b083ff96e941fdeb8540bee01cdf03742ef5c
Øxonovo <mahalosaint@gmail.com>
2025-03-10 00:24:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,12 +16,3 @@ menu_btn.addEventListener('click', function() {
     mobile_menu.classList.toggle('is-active');
 });
 
-  // Function to detect mobile or tablet devices
-function isMobileOrTablet() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
-// Redirect mobile and tablet users to a different webpage
-if (isMobileOrTablet()) {
-  window.location.href = "Bitcoin.html"; // Replace with your desired URL
-}
